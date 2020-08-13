5842772530e6219a72c1765fa0c32f09ea883eb6
Huy Truong <huy.truong@mevry.com>
2020-08-13 22:53:47 +0700

    create foreign key & automate cascade delete

--- DatingApp-API/Data/DataContext.cs
+++ DatingApp-API/Data/DataContext.cs
@@ -10,5 +10,6 @@ namespace DatingAppAPI.Data
 
         public DbSet<Value> Values { get; set;}
         public DbSet<User> Users { get; set;}
+        public DbSet<Photo> Photos {get;set;}
     }
 }
