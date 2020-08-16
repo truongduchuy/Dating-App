506ba7242b0ac338a4d070147188d438be66bb5e
Huy Truong <huy.truong@mevry.com>
2020-08-16 16:18:10 +0700

    attach token into header for request

---
+++ DatingApp-Angular/src/app/members/member-detail/member-detail.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../_service/user.service';
+import { AlertifyService } from '../../_service/alertify.service';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../../_models/user';
+
+@Component({
+  selector: 'app-member-detail',
+  templateUrl: './member-detail.component.html',
+  styleUrls: ['./member-detail.component.scss'],
+})
+export class MemberDetailComponent implements OnInit {
+  user: User;
+  constructor(
+    private userService: UserService,
+    private alertifyService: AlertifyService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.loadUser();
+  }
+
+  loadUser() {
+    this.userService.getUser(+this.route.snapshot.params['id']).subscribe(
+      (user: User) => {
+        this.user = user;
+      },
+      (error) => this.alertifyService.error(error)
+    );
+  }
+}
