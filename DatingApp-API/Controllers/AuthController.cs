2ec807005140d2d9fbab1b5701a95678ea160576
Huy Truong <huy.truong@mevry.com>
2020-09-12 23:28:09 +0700

    complete register & set default image

--- DatingApp-API/Controllers/AuthController.cs
+++ DatingApp-API/Controllers/AuthController.cs
@@ -37,13 +37,13 @@ namespace DatingApp_API.Controllers
                 return BadRequest("Username already exists");
             }
 
-            var userToCreate = new User {
-                Username = userForRegisterDto.Username
-            };
+            var userToCreate =  _mapper.Map<User>(userForRegisterDto);
 
             var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
-            return StatusCode(201);
+            var userToReturn = _mapper.Map<UserForRegisterDto>(createdUser);
+
+            return CreatedAtRoute("GetUser", new { Controller = "Users", id = createdUser.Id}, userToReturn); 
         }
 
         [HttpPost("login")]
